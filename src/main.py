d8cce64340677061a1a552331aefdc476c8072c7
Mike <michaellaishleys@gmail.com>
2024-05-03 16:51:28 +1000

    made character avatar input, allows movement around grid

--- src/main.py
+++ src/main.py
@@ -1,57 +1,22 @@
-import grid
+import easygrid
+import curses
 
-# while True:
-#     print(grid.fourByFour)
-#     move = input('Where do you want to move? Up, Down, Left or Right? \n> ').lower()
-#     if move == 'Right':
-#         if spaceEmpty:
-#             print('Theres nothing here')
+name = input("Enter your character name: ")
 
-# print(grid.vector[0])
+def print_grid(grid, object_position):
+    grid[object_position[0]][object_position[1]] = name[0].upper()
 
-# four_by_four = [['  ' for _ in range(4)] for _ in range(4)]
-
-# position = [1, 1]
-
-# movement = [1, 0]
-
-# def move_in_grid(position, movement, four_by_four):
-#     grid_height = len(four_by_four)
-#     grid_width = len(four_by_four[0])
-
-#     new_y = position[0] + movement[0]
-#     new_x = position[1] + movement[1]
-
-#     if 0 <= new_y < grid_height and 0 <= new_x < grid_width:
-#         position[0] = new_y
-#         position[1] = new_x
-#         return True
-#     else:
-#         return False
-    
-# if move_in_grid(position, movement, four_by_four):
-#     print("You moved to:", position)
-# else:
-#     print("You can't move there")
-
-def print_grid(four_by_four, object_position):
-    four_by_four[object_position[0]][object_position[1]] = 'P'
-
-    print("+" + "---" * 3 + "+" for _ in range(len(four_by_four[0])))
-
-    for row in four_by_four:
-        print("[  ]", end="")
+    for row in grid:
+        print("", end="")
         for element in row:
-            print(" " + element + " ", end="")
-        print("[  ]")
+            print("[ " + element + " ]", end="")
+        print("")
 
-    print("+" + "---" * 3 + "+" for _ in range(len(four_by_four[0])))
+    grid[object_position[0]][object_position[1]] = ' '
 
-    four_by_four[object_position[0]][object_position[1]] = ' '
-
-def move_object(object_position, movement, four_by_four):
-    grid_height = len(four_by_four)
-    grid_width = len(four_by_four[0])
+def move_object(object_position, movement, grid):
+    grid_height = len(grid)
+    grid_width = len(grid[0])
 
     new_y = object_position[0] + movement[0]
     new_x = object_position[1] + movement[1]
@@ -63,13 +28,28 @@ def move_object(object_position, movement, four_by_four):
     else:
         return False
     
-four_by_four = [[' ' for _ in range(4)] for _ in range(4)]
+grid = [[' ' for _ in range(4)] for _ in range(4)]
 
-object_position = [1, 1]
+object_position = [0, 0]
 
-movement = [1, 0]
+movement = [0, 0]
 
-if move_object(object_position, movement, four_by_four):
-    print_grid(four_by_four.copy(), object_position)
-else:
-    print("You can't move there")
+while True:
+    print_grid(grid.copy(), object_position)
+
+    direction = input("Where would you like to move? (up, down, left, right): ").lower()
+
+    if direction == "up":
+        movement = [-1, 0]
+    elif direction == "down":
+        movement = [1, 0]
+    elif direction == "left":
+        movement = [0, -1]
+    elif direction == "right":
+        movement = [0, 1]
+    else:
+        print("You must remain in the grid!")
+        continue
+
+    if not move_object(object_position, movement, grid):
+        print("You must remain in the grid!")
